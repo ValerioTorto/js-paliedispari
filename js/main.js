d120b2a2e6fd12f78e7d726815982a1931253057
ValerioTorto <valerio.tortorelli@gmail.com>
2023-11-20 16:21:45 +0100

    END

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 //PALINDROMA
 
 //Chiedo all'utente una parola
-
+/*
 let word = prompt("inserisci una parola");
 let reversedword = reverseString(word);
 
@@ -18,32 +18,43 @@ else {
 //Funzione per invertire la stringa
 function reverseString(str) {
     return str.split("").reverse().join("");
-}
+}*/
 
 //Pari o dispari
 //INFORMAZIONI DA CHIEDERE ALL'UTENTE
 let choice = prompt("scegli se pari o dispari");
 let number = prompt("inserisci un numero da 1 a 5");
-
+console.log("scelto dall'utente: " + choice);
 //TRASFORMO IN UN NUMERO
 number = parseInt(number);
+console.log("numero utente: " + number);
+
+let randNum = randomNumberGeneration(1, 5)
+console.log("numero casuale " + randNum)
+
+let winner = numSum(number, randNum, choice)
 
 
-//GENERO UN NUMERO RANDOM DA 1 A 5
-function genRanNum1To5(maxNumber) {
-    let randomNumber = Math.floor(Math.random() * 6 (maxNumber + 1));
-    return randomNumber;
+//GENERO NUMERO CASUALE 
+function randomNumberGeneration(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-//let randNum = math.floor(math.random() * 6)
-
-let somma = randomNumber + number;
-
-if(somma % 2 == 0 && choice = "pari" ){
-    console.log("HAI VINTO");
-    else if (somma % 2 !==0 )
-    console.log("HAI VINTO")
+function numSum(num1, num2, stri){
+    let sum = num1 + num2 
+    if (sum % 2 == 0 && stri =="pari"){
+        console.log(`${sum} hai vinto!`)
+    }else if(sum % 2 == 0 && stri == "dispari"){
+            console.log(`${sum} hai perso!`)
+    }else if (sum % 2 !==0 && stri =="dispari"){
+        console.log(`${sum} hai vinto!`)
+    }else if (sum % 2 !==0 && stri == "pari"){
+        console.log(`${sum} hai perso!`)
+    }
+        
+    }
     
 
-}
+
+
 
 
